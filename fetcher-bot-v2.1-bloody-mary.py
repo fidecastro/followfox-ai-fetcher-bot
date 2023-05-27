8d56bb15e2613f775ddf43fea3b29dac087f613d
fidecastro <felipe.infante.de.castro@gmail.com>
2023-05-27 17:04:18 -0400

    Add files via upload

--- fetcher-bot-v2.1-bloody-mary.py
+++ fetcher-bot-v2.1-bloody-mary.py
@@ -10,6 +10,7 @@ import base64
 import io
 import json
 import time
+from filelock import FileLock, Timeout
 
 TOKEN = os.getenv('DISCORD_BOT_TOKEN_BM')  # Fetch token from environment variable
 SERVER_ID = [1105589864453394472]  # Followfox AI Testbed
@@ -17,6 +18,21 @@ MAX_IMAGES = 4  # Maximum number of images to generate
 base_url = "http://127.0.0.1:7860"
 img2img_switch = False # Set to True to enable Img2Img generation
 img2img_upscaling = False # if set to True AND img2img_swith is also True, will bypass Img2img+Controlnet and will just do Ultra Upscaling
+
+#MODEL_NAME = "vodkaByFollowfoxAI_v10.safetensors [d1d133610b]"
+#MODEL_NAME = "vodkaByFollowfoxAI_v20.safetensors [5e1d7de310]"
+#MODEL_NAME = "4_vodka_v3_100.ckpt [8dc24d7ba9]"
+MODEL_NAME = "1_bloody_mary_v1.safetensors [ac7d34f7c9]"
+
+#BOT_NAME = 'create-vodka-v3'
+BOT_NAME = 'create-bloodymary'
+
+#BOT_DESCRIPTION = 'Create upscaled images using FollowFox AI Vodka V3'
+BOT_DESCRIPTION = 'Create upscaled images using FollowFox AI Bloody Mary'
+
+# Filelock Retry settings
+RETRY_DELAY_SECONDS = 5
+MAX_RETRIES = 100
 
 # These base prompts will be added to the input text to generate the images. The user will NOT see these prompts as additions to their input text.
 base_negative_prompt = ", worst quality, deformed, low quality, bad"
@@ -26,6 +42,8 @@ intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+sem = asyncio.Semaphore(1)  # Create a semaphore that allows only one concurrent operation
 
 async def process_image(session, base_url, image_b64, payload, image_number):
     image = Image.open(io.BytesIO(base64.b64decode(image_b64.split(",",1)[0])))
@@ -40,10 +58,12 @@ async def process_image(session, base_url, image_b64, payload, image_number):
         image.save(bio, 'PNG', pnginfo=pnginfo)
         bio.seek(0)  # reset file pointer to the beginning
     if img2img_switch == False: 
-        print("Avoiding Txt2Img processing...")
+        timestamp = time.time()
+        print(f'    {timestamp:.2f}: '+ MODEL_NAME + '- Avoiding Txt2Img processing...')
         return discord.File(bio, filename=f'image{image_number+1}.png')  # directly return a Discord file object
     else: 
-        print("Processing image with Img2Img...")
+        timestamp = time.time()
+        print(f'    {timestamp:.2f}: '+ MODEL_NAME + '- Processing image with Img2Img...')
         with open('payload-img2img.json', 'r') as json_file:
             payload_upscale = json.load(json_file)
 
@@ -71,7 +91,8 @@ async def process_image(session, base_url, image_b64, payload, image_number):
         async with session.post(f'{base_url}/sdapi/v1/img2img', json=payload_upscale) as response3:
             r = await response3.json()
             j = r['images'][0]
-            print(f"IMG2IMG image {j[-32:]} for TXT2IMG image {image_b64[-32:]}")
+            timestamp = time.time()            
+            print(f'    {timestamp:.2f}: '+ MODEL_NAME + '- IMG2IMG image {j[-32:]} for TXT2IMG image {image_b64[-32:]}')
             image_upscale = Image.open(io.BytesIO(base64.b64decode(j)))
             png_payload_upscale = {
                 "image": "data:image/png;base64," + j
@@ -91,35 +112,53 @@ async def generate_images(session, base_url, payload, image_number):
         r = await response.json()
         file_objects = []  # This list will store all the file objects that are generated
         for i in r['images']:
-            print(f"TXT2IMG image {i[-32:]}")
+            timestamp = time.time()
+            print(f'    {timestamp:.2f}: '+ MODEL_NAME + f'- TXT2IMG image {i[-32:]}')
             file_object = await process_image(session, base_url, i, payload, image_number)  
             file_objects.append(file_object)  # Add the generated file object to the list
         return file_objects  # Return the list of file objects
 
 
 async def fetch_images(input_text, input_negative_text):
-    # Read the dictionary from the Payload JSON file
     with open('payload-txt2img.json', 'r') as json_file:
         payload = json.load(json_file)
-
+        
     payload["prompt"] = input_text  # Introduce to payload the input text
     payload["negative_prompt"] = input_negative_text  # Introduce to payload the input text
-    base_url = "http://127.0.0.1:7860"
-    
-    async with aiohttp.ClientSession() as session:        
+
+    async with aiohttp.ClientSession() as session:
+        # Get the existing options
         async with session.get(f'{base_url}/sdapi/v1/options') as resp:
             opt_json = await resp.json()
-            opt_json['sd_model_checkpoint'] = "1_bloody_mary_v1.safetensors [ac7d34f7c9]"
-            #opt_json['sd_model_checkpoint'] = "vodkaByFollowfoxAI_v10.safetensors [d1d133610b]"
-            #opt_json['sd_model_checkpoint'] = "vodkaByFollowfoxAI_v20.safetensors [5e1d7de310]"
-            #opt_json['sd_model_checkpoint'] = "4_vodka_v3_100.ckpt [8dc24d7ba9]"
 
-        # Send the updated options back to the server.
-        async with session.post(f'{base_url}/sdapi/v1/options', json=opt_json) as resp:
-            if resp.status != 200:
-                print(f'Failed to update model: {resp.status}')
-            else:
-                print('Model updated successfully')
+        # Retry logic for updating the options
+        for attempt in range(MAX_RETRIES):
+            timestamp = time.time()
+            print(f'    {timestamp:.2f}: '+ MODEL_NAME + f'- Waiting to check model...')            
+            async with sem:  # Protect the operation with a semaphore
+                try:
+                    with FileLock("options.lock", timeout=1):  # Try to acquire the lock
+                        # Update the options with the correct SD model
+                        opt_json['sd_model_checkpoint'] = MODEL_NAME
+                        
+                        # Re-POST the entire options object
+                        async with session.post(f'{base_url}/sdapi/v1/options', json=opt_json) as resp:
+                            if resp.status != 200:
+                                timestamp = time.time()
+                                print(f'    {timestamp:.2f}: '+ MODEL_NAME + f'- Failed to update model: {resp.status}')
+                            else:
+                                timestamp = time.time()
+                                print(f'    {timestamp:.2f}: '+ MODEL_NAME + '- Model updated successfully')
+                                break  # If we successfully updated the options, break out of the retry loop
+                except Timeout:
+                    timestamp = time.time()
+                    print(f'    {timestamp:.2f}: '+ MODEL_NAME + f'- Lock is in use. Retrying after {RETRY_DELAY_SECONDS} seconds...')
+                    await asyncio.sleep(RETRY_DELAY_SECONDS)  # Wait before retrying
+                else:
+                    timestamp = time.time()
+                    print(f'    {timestamp:.2f}: '+ MODEL_NAME + f'- Failed to update options after {MAX_RETRIES} attempts')
+                    break  # If we've reached the max number of retries, give up
+
         tasks = [generate_images(session, base_url, payload, k) for k in range(MAX_IMAGES)]
         file_objects = await asyncio.gather(*tasks)  # This will be a list of lists
         return [item for sublist in file_objects for item in sublist]  # Flatten the list
@@ -129,21 +168,19 @@ async def fetch_images(input_text, input_negative_text):
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-@bot.slash_command(name="create-bloodymary", guild_ids=SERVER_ID, description="Create upscaled images using FollowFox AI Bloody Mary", interaction_response_message="Creating images. Please wait...")
+@bot.slash_command(name=BOT_NAME, guild_ids=SERVER_ID, description=BOT_DESCRIPTION, interaction_response_message="Serving images. Please wait...")
 async def create(ctx, *, prompt: str, negative_prompt: str = ""):
     start_time = time.time()
     print(f"{start_time:.2f}: Fetcher received request. Sending to SD API...")
-    await ctx.respond(f"Serving images for *'{prompt}' --neg '{negative_prompt}'*. Please wait...")
+    await ctx.respond(f"Serving images for *{prompt} --neg {negative_prompt}*. Please wait...")
     images = await fetch_images(prompt + base_positive_prompt, negative_prompt + base_negative_prompt)    
     files_dict = {f'file{i}': image for i, image in enumerate(images)}
-    await ctx.send(f"*'{prompt}' --neg '{negative_prompt}', by {ctx.author.mention}:*", files=files_dict.values())       
+    await ctx.send(f"*{prompt} --neg {negative_prompt}, by {ctx.author.mention}:*", files=files_dict.values())       
     timestamp = time.time()
-    print(f"    {timestamp:.2f}: '{prompt}' --neg '{negative_prompt}', by @{ctx.author}.")
+    print(f"    {timestamp:.2f}: "+ MODEL_NAME + f"{prompt} --neg {negative_prompt}, by @{ctx.author}.")
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"    Fetcher request completed in {elapsed_time:.2f} seconds.")
 
 
 bot.run(TOKEN)
-
-
